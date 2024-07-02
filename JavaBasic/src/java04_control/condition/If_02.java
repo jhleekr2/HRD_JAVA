cad22df2ab50f935f2241f3cdd660ee463236256
jhleekr2 <jhleekr2@gmail.com>
2024-07-02 09:14:15 +0900

    2024-07-02

---
+++ JavaBasic/src/java04_control/condition/If_02.java
@@ -0,0 +1,18 @@
+package java04_control.condition;
+
+public class If_02 {
+	public static void main(String[] args) {
+		int num = 10;
+		
+		if(num == 15) {
+			System.out.println("HI");
+		}
+		System.out.println("HELLO");
+		}
+	
+	//----------------------------------------------------------
+	
+	{
+		
+	}
+}
