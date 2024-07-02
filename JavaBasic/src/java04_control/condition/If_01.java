abdaba3a512cc767ac89f79fc6799f076691557a
jhleekr2 <jhleekr2@gmail.com>
2024-07-02 10:03:18 +0900

    2024-07-02

---
+++ JavaBasic/src/java04_control/condition/If_01.java
@@ -0,0 +1,26 @@
+package java04_control.condition;
+
+public class If_01 {
+	public static void main(String[] args) {
+		
+		//조건문 - if문
+		
+//		if ( 조건식 ) {
+			// 코드 작성
+			
+			// 조건식이 참(true)일 때만 실행하는 코드를 작성하는 영역
+//		}
+		//-------------------------------------------------------------
+		
+		if (true) {
+			System.out.println("이거 실행될까여?");
+		}
+		
+		if (false ) {
+			System.out.println("여기는? 여기는 어때여");
+		}
+		
+		System.out.println("if문 바깥쪽!");
+		System.out.println("이쪽은 실행될까여?????");
+	}
+}
