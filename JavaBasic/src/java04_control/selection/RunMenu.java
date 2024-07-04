901c0d8096ac4456103a9c919a01a6a721f0951d
unknown <jhleekr2@gmail.com>
2024-07-04 13:29:52 +0900

    2024-07-04

---
+++ JavaBasic/src/java04_control/selection/RunMenu.java
@@ -0,0 +1,28 @@
+package java04_control.selection;
+
+public class RunMenu {
+	
+
+	//메소드 정의(define)
+	public void printHello() {
+		System.out.println("Hello World");
+	}
+	
+	public void printName() {
+		System.out.println("Name");
+
+	}
+	
+	public void printGender() {
+		System.out.println("M");
+
+	}
+	
+	public void printExit() {
+	}
+	
+	public void printDefault() {
+		System.out.println("메뉴를 다시 선택하세요.");
+		
+	}
+}
