c4ec33b684b0619d2c3a9c7469913241c842a57e
jhleekr2 <jhleekr2@gmail.com>
2024-07-06 05:52:58 +0900

    2024-07-06

--- JavaBasic/src/java04_control/condition/If_01.java
+++ JavaBasic/src/java04_control/condition/If_01.java
@@ -3,6 +3,29 @@ package java04_control.condition;
 public class If_01 {
 	public static void main(String[] args) {
 		
+//		- 제어문(Control Statement)
+//		- 프로그램의 기본 진행 흐름을 변경(제어)하는 구문을 뜻한다.
+//		- main() 메소드에서 시작하여 코드를 순서대로 진행하는 흐름을
+//		변경할 수 있다.
+//		- 사용자와 프로그램이 상호작용하는 것에 따라 프로그램 진행이 \
+//		달라지도록 코드를 작성할 수 있다.
+//
+//		- 제어문의 종류
+//		-조건문, condition
+//		if, else if, else
+//
+//		-반복문, loop
+//		for, while, do-while
+//
+//		-선택문, selection
+//		switch
+//
+//		-기타 제어문
+//		break, continue
+//
+//		- 자바는 java4_control과 java4_control.condition, java4_control.loop를
+//		완전 별개의 패키지로 간주한다.
+//		
 		//조건문 - if문
 		
 //		if ( 조건식 ) {
