e2d1640ab05634d940cdee8443768b7bcea3800d
unknown <jhleekr2@gmail.com>
2024-07-10 08:52:00 +0900

    2024-07-10

--- JavaBasic/src/java06_class/UML/Car.java
+++ JavaBasic/src/java06_class/UML/Car.java
@@ -2,6 +2,34 @@ package java06_class.UML;
 
 public class Car {
 	
+//	클래스 다이어그램(Class Diagram) - 모든 언어에서 쓰는 표준적인 표기법
+//	- 클래스의 내부 구조, 유형, 클래스 간 관계 등을 그림으로 표현한 것
+//	- 접근제한자를 표현하는 기호
+//
+//	+: public
+//
+//	#: protected
+//
+//	~: default
+//
+//	-: private
+//
+//	- 변수의 데이터타입, 메소드의 반환타입, 메소드의 매개변수 타입을 클래스 다이어그램의 표기법에 맞게 작성한다
+//	- 데이터타입을 문법요소 뒤에 : 을 붙이고 작성한다.
+//	ex) 멤버필드 표기
+//	private int num; → -num : int
+//	
+//	public String name; → +name : String
+//
+//	ex) 멤버메소드 표기
+//	public void display(int n1, int n2) {}
+//
+//	→ + display( n1:int, n2:int) : void
+//	→ + display( int, int) : void (매개변수 이름을 맘대로 정할 수 있도록 한 방법)
+//
+//	UML 웹 도구 사이트
+//	[draw.io](http://draw.io) 접속
+//	
 	// - model : String
 	// - color : String
 	private String model; // 자동차 모델
