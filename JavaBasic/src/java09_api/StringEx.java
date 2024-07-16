766717aeabf3339818c506846224d0f0457c3499
unknown <jhleekr2@gmail.com>
2024-07-17 02:56:00 +0900

    2024-07-17

--- JavaBasic/src/java09_api/StringEx.java
+++ JavaBasic/src/java09_api/StringEx.java
@@ -1,21 +1,71 @@
 package java09_api;
 
-public class StringEx {
+import java.util.Locale;
 
+public class StringEx {
+//		String 클래스
+//		- 패키지: java.lang
+//		- 문자열을 표현하는 클래스
+//		- 문자열 객체, 문자열 리터럴 상수 모두 String 클래스 타입으로 적용된다
+//		
+//		문자열 객체 사용 방법
+//		- 방법1. 리터럴 상수
+//		ex)	String str1 = "Apple";
+//		- 방법2. String 객체 생성
+//		ex)	String str2 = new String("Banana");
+	
 	public static void main(String[] args) {
-		
 		
 		String str1 = "Apple"; //String 리터럴 상수
 		String str2 = new String("Banana"); //String 객체 생성 (동적 할당)
 		
 		//-------------------------------------------------------
 		
+		System.out.println("--- length() -------");
 		
+		System.out.println("str1의 길이 : " + str1.length()); //5
+		System.out.println("str2의 길이 : " + str2.length()); //6
 		
+		System.out.println("\"Cherry\"의 길이 : " + "Cherry".length());
+		
+		//-------------------------------------------------------
+		//QUIZ.
+		
+		System.out.println("\n--- toLowerCase() -------");
+		//str1 문자열의 모든 알파벳을 소문자로 변경한 결과를 반환한다.
+		String lower = str1.toLowerCase();
+		System.out.println("lower : " + lower);
+		
+		System.out.println("\n--- toUpperCase() -------");
+		//str2 문자열의 모든 알파벳을 대문자로 변경한 결과를 반환한다.
+		String upper = str2.toUpperCase();
+		System.out.println("upper : " + upper);
+		
+		System.out.println("\n--- charAt(int) -------");
+		//str1의 3번째 인덱스 값을 추출한다
+		char alpha = str1.charAt(3);
+		System.out.println("3번째 인덱스 값 : " + alpha);
+
+		System.out.println("\n--- replace(char, char) -------");
+		//str2 문자열의 'n'을 'X'로 변환한다
+		String replace = str2.replace( 'n', 'X' );
+		System.out.println( replace );
+		
+		String replace2 = str2.replace( "n", "X" );
+		System.out.println( replace2 );
+
+		System.out.println("\n--- contains() -------");
+		//특정 문자열을 포함하고 있는지 확인한다
+		boolean b = str1.contains("pp");
+		System.out.println( b );
+
 		System.out.println("\n--- substring() -------");
 		//문자열의 일부를 발췌한다
 		
 		String sub1 = str1.substring(2);
 		System.out.println("sub1 : " + sub1);
+		
+		String sub2 = str1.substring(1,3); //endIndex인 3 인덱스는 제외한다.
+		System.out.println("sub2 : " + sub2);
 	}
 }
--- JavaBasic/src/java09_api/WrapperEx.java
+++ JavaBasic/src/java09_api/WrapperEx.java
@@ -39,6 +39,8 @@ public class WrapperEx {
 		//----------------------------------------------------
 		
 		//** Wrapper 클래스의 생성자들은 Deprecate 되어 있다
+		//대신 valueOf()를 사용하도록 권장하고 있다.
+		//이유 - Wrapper 클래스의 생성자 쓰는 것이 성능상 손실이 있음
 		
 		Integer iVal3 = new Integer( "300" );
 		Integer iVal4 = new Integer( 300 );
@@ -48,12 +50,50 @@ public class WrapperEx {
 		//** int 타입으로 Integer타입값 대입할 수 있음
 		// -> int, Integer를 서로 같은 타입이라고 생각하면 된다
 		
-		//int i1 = iVal1;
-		//int i2 = iVal2;
+		int i1 = iVal1;
+		int i2 = iVal2;
 		
 		//----------------------------------------------------
 		
 		//자료형에 대한 정보
+		
+		System.out.println("BYTES : " + Integer.BYTES); // 4 (byte)
+		System.out.println("SIZE : " + Integer.SIZE); // 32 (bit)
+		
+		System.out.println("Double BYTES : " + Double.BYTES); // 8 (byte)
+		
+		System.out.println("Boolean BYTES : " + Boolean.TRUE); // true
+		System.out.println("Boolean BYTES : " + Boolean.FALSE); //false
+		
+		System.out.println("------------");
+		
+		System.out.println("MIN_VALUE : " + Integer.MIN_VALUE);
+		System.out.println("MAX_VALUE : " + Integer.MAX_VALUE);
+
+		System.out.println("MAX_VALUE : " + Long.MAX_VALUE);
+		
+		System.out.println("------------");
+		
+		//** parse
+		//	-> (문법적으로) 분석한다
+		//	-> 파싱
+				
+		//	-> 일정한 패턴을 이용하여 원하는 형식의 데이터를 추출한다
+		
+		//-----------------------------------------------------------------------
+		
+//		Integer.parseInt("문자열") : int
+		// -> "문자열"에서 int타입 데이터를 추출하여 반환한다.
+		
+		//기본형 <- 문자열 ( Wrapper.parseType( String ) )
+		int number = Integer.parseInt("987");
+		double dNum = Double.parseDouble("323.345");
+		boolean bData = Boolean.parseBoolean("true");
+		
+		//문자열 <- 기본형 ( String.valueOf( data ) )
+		String iStr = String.valueOf( 12356 );
+		String bStr = String.valueOf( false );
+
 	}
 	
 }
